5cfe487c1836b64475844875542187f2ae9d2fb2
01-ERFA <atukdibe@gmail.com>
2022-12-08 15:06:15 -0300

    Tasks && Test view added

--- routes/site.py
+++ routes/site.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request
+from models.tasks import Task
+from utils.session import session
+
 
 site = Blueprint('site', __name__)
-
 
 @site.route('/')
 def sitemap():
@@ -12,6 +14,16 @@ def sitemap():
 def docs_page():
     return render_template('docs.html')
 
-@site.route('/testing')
-def test_api():
-    return render_template('test.html')
+@site.route('/tasks', methods=['GET'])
+def get_all_tasks():
+    tasks = Task.query.all()
+    return render_template('test.html', tasks=tasks)
+
+@site.route('/tasks', methods=['POST'])
+def save_tasks():
+    new_task = Task(title=request.form['title'], description=request.form['description'])
+    
+    session.add(new_task)
+    session.commit()
+    tasks = Task.query.all()
+    return render_template('test.html', tasks=tasks)
