609f3a98be9f3603a8a0108098d16e0e3e5b4494
01-ERFA <atukdibe@gmail.com>
2022-12-07 22:12:32 -0300

    arreglado los templates html

--- routes/home.py
+++ routes/home.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, render_template
-from models.tasks import Task
 
 home = Blueprint('api', __name__)
 
 @home.route('/')
 def sitemap():
     return render_template("home.html")
-
