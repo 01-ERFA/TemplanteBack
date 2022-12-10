5c13969f12bc61654594358e89c90f20b7c67804
01-ERFA <atukdibe@gmail.com>
2022-12-09 21:43:59 -0300

    update animation in scripts.py

--- utils/scripts.py
+++ utils/scripts.py
@@ -12,29 +12,32 @@ def space(num):
         print('\n')
         b+=1
 
-def animation(large, start, message, sequence):
-    empty = "-" * large
+def animation(large, start, short_end , message, sequence):
+    if start != 0:
+        empty = "#" * start + "-" * (large-start)
+    else:
+        empty = "-" * large
     character = "#"
-    b = 0
-    while start < large and b <large:
+    b = start
+    while short_end < large and b <large:
         aux = list(empty)
         aux[b] = character
         empty = ''.join(aux)
         print(f'[{empty}] {(b+1)*2}%', end='\r')
-        if b == large-1 or start == 49:
+        if b == large-1 or short_end == large-1:
             space(1)
             print(message)
         time.sleep(sequence)
         b+=1
-        start+=1
+        short_end+=1
 
 def create_env():
     if os.path.exists(path+'.env'):
-        animation(50, 0, scripts['messages']['create_env']['env_failed'], 0.03)
+        animation(50, 0, 0, scripts['messages']['create_env']['env_failed'], 0.03)
     else:
         env = open('.env', 'w')
         env.write(scripts['content']['create_env'])
-        animation(50, 0, scripts['messages']['create_env']['env_success'], 0.03)
+        animation(50, 0, 0, scripts['messages']['create_env']['env_success'], 0.03)
 
 action = 0
 while action < len(args.command):
@@ -44,10 +47,10 @@ while action < len(args.command):
             try:
                 create_env()
             except:
-                animation(50, 32, scripts['messages']['create_env']['env_failed_unexpected'], 0.07)
+                animation(50, 0, 32, scripts['messages']['create_env']['env_failed_unexpected'], 0.07)
         case 'animation':
             space(1)
-            animation(50, 0, "animation", 0.6)
+            animation(50, 5, 0, "animation", 0.6)
         case _:
             print('\n')
             print(r"ERROR: could not find command: %r" %command)
